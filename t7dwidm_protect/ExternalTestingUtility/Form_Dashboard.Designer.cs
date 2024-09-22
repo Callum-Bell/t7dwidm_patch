28b9b20955da48d3d1cce22284373c81fd90ada8
Callum-Bell <insanecalgta@gmail.com>
2024-09-22 21:17:13 +0100

    Add project files.

---
+++ t7dwidm_protect/ExternalTestingUtility/Form_Dashboard.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace t7dwidm_protect
+{
+    partial class Form_Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Dashboard));
+            this.panel_TitleBar = new System.Windows.Forms.Panel();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.panel_MainButtons = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt_NetworkPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.NameChangeTimer = new System.Windows.Forms.Timer(this.components);
+            this.netpasstimer = new System.Windows.Forms.Timer(this.components);
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.panel_MainButtons.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_TitleBar
+            // 
+            this.panel_TitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(199)))), ((int)(((byte)(255)))));
+            this.panel_TitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_TitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panel_TitleBar.Name = "panel_TitleBar";
+            this.panel_TitleBar.Size = new System.Drawing.Size(432, 1);
+            this.panel_TitleBar.TabIndex = 0;
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold);
+            this.lbl_Title.ForeColor = System.Drawing.Color.White;
+            this.lbl_Title.Location = new System.Drawing.Point(12, 17);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(322, 40);
+            this.lbl_Title.TabIndex = 1;
+            this.lbl_Title.Text = "Black Ops 3 - Zombies";
+            // 
+            // panel_MainButtons
+            // 
+            this.panel_MainButtons.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(38)))), ((int)(((byte)(46)))));
+            this.panel_MainButtons.Controls.Add(this.label3);
+            this.panel_MainButtons.Controls.Add(this.panel2);
+            this.panel_MainButtons.Controls.Add(this.label2);
+            this.panel_MainButtons.Controls.Add(this.panel1);
+            this.panel_MainButtons.Location = new System.Drawing.Point(21, 62);
+            this.panel_MainButtons.Name = "panel_MainButtons";
+            this.panel_MainButtons.Size = new System.Drawing.Size(389, 150);
+            this.panel_MainButtons.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(146)))), ((int)(((byte)(154)))));
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "PASSWORD";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(53)))), ((int)(((byte)(63)))));
+            this.panel2.Controls.Add(this.txt_NetworkPassword);
+            this.panel2.Location = new System.Drawing.Point(16, 95);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(353, 38);
+            this.panel2.TabIndex = 7;
+            // 
+            // txt_NetworkPassword
+            // 
+            this.txt_NetworkPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(53)))), ((int)(((byte)(63)))));
+            this.txt_NetworkPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_NetworkPassword.ForeColor = System.Drawing.Color.White;
+            this.txt_NetworkPassword.Location = new System.Drawing.Point(11, 11);
+            this.txt_NetworkPassword.Name = "txt_NetworkPassword";
+            this.txt_NetworkPassword.PasswordChar = '*';
+            this.txt_NetworkPassword.Size = new System.Drawing.Size(333, 16);
+            this.txt_NetworkPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(146)))), ((int)(((byte)(154)))));
+            this.label2.Location = new System.Drawing.Point(12, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "USERNAME";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(53)))), ((int)(((byte)(63)))));
+            this.panel1.Controls.Add(this.txt_Username);
+            this.panel1.Location = new System.Drawing.Point(16, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 38);
+            this.panel1.TabIndex = 1;
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(53)))), ((int)(((byte)(63)))));
+            this.txt_Username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Username.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Username.ForeColor = System.Drawing.Color.White;
+            this.txt_Username.Location = new System.Drawing.Point(11, 11);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(333, 16);
+            this.txt_Username.TabIndex = 3;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(42)))), ((int)(((byte)(49)))));
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(100)))), ((int)(((byte)(54)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(399, 1);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(33, 31);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // NameChangeTimer
+            // 
+            this.NameChangeTimer.Interval = 500;
+            // 
+            // netpasstimer
+            // 
+            this.netpasstimer.Interval = 1500;
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lbl_Status.ForeColor = System.Drawing.Color.White;
+            this.lbl_Status.Location = new System.Drawing.Point(4, 218);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(120, 15);
+            this.lbl_Status.TabIndex = 5;
+            this.lbl_Status.Text = "Game Not Launched";
+            // 
+            // Form_Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(42)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(432, 241);
+            this.Controls.Add(this.lbl_Status);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.panel_MainButtons);
+            this.Controls.Add(this.lbl_Title);
+            this.Controls.Add(this.panel_TitleBar);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form_Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T7Patch Loader";
+            this.Load += new System.EventHandler(this.Form_Dashboard_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form_Dashboard_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form_Dashboard_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form_Dashboard_MouseUp);
+            this.panel_MainButtons.ResumeLayout(false);
+            this.panel_MainButtons.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_TitleBar;
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Panel panel_MainButtons;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.TextBox txt_NetworkPassword;
+        private System.Windows.Forms.Timer NameChangeTimer;
+        private System.Windows.Forms.Timer netpasstimer;
+        private System.Windows.Forms.Label lbl_Status;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ t7dwidm_protect/ExternalTestingUtility/Form_Dashboard.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using t7dwidm_protect.Cheats;
+
+namespace t7dwidm_protect
+{
+    public partial class Form_Dashboard : Form
+    {
+        private Action UpdateProtection;
+        private PlayerSettings Settings = new PlayerSettings();
+
+        public Form_Dashboard()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form_Dashboard_Load(object sender, EventArgs e)
+        {
+            UpdateProtection = __updateprotection;
+            if (!File.Exists("settings.json"))
+            {
+                CommitSettings();
+            }
+            LoadSettings();
+            new Task(() =>
+            {
+                try
+                {
+                    PollProcesses();
+                }
+                catch (Exception ex)
+                {
+                    // Log or display the exception
+                    MessageBox.Show(ex.Message);
+                }
+            }).Start();
+
+            NameChangeTimer.Tick += NameChangeTimer_Tick;
+            netpasstimer.Tick += Netpasstimer_Tick;
+            DoubleBuffered = true;
+        }
+
+        private void Netpasstimer_Tick(object sender, EventArgs e)
+        {
+            Settings.NetworkPassword = txt_NetworkPassword.Text;
+            CommitSettings();
+            if (BlackOps3.IsGamePresent() && moduleHandle != null)
+            {
+                BlackOps3.Game.Call(moduleHandle["SetNetworkPassword"], Settings.NetworkPassword);
+            }
+            netpasstimer.Stop();
+        }
+
+        private void NameChangeTimer_Tick(object sender, EventArgs e)
+        {
+            Settings.Playername = txt_Username.Text;
+            if (Settings.Playername.Length >= 16)
+            {
+                Settings.Playername = Settings.Playername.Substring(0, 15);
+            }
+            CommitSettings();
+            if (BlackOps3.IsGamePresent() && moduleHandle != null)
+            {
+                BlackOps3.Game.Call(moduleHandle["SetPlayerName"], Settings.Playername);
+            }
+            NameChangeTimer.Stop();
+        }
+
+        private void CommitSettings()
+        {
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = new LowerNamePol(),
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            };
+            File.WriteAllBytes("settings.json", Encoding.ASCII.GetBytes(JsonSerializer.Serialize(Settings, typeof(PlayerSettings), serializeOptions)));
+        }
+
+        private void LoadSettings()
+        {
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = new LowerNamePol(),
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            };
+            Settings = (PlayerSettings)JsonSerializer.Deserialize(File.ReadAllBytes("settings.json"), typeof(PlayerSettings), serializeOptions);
+
+            if (Settings.Playername.Length >= 16)
+            {
+                Settings.Playername = Settings.Playername.Substring(0, 15);
+            }
+
+            txt_Username.Text = Settings.Playername;
+            txt_NetworkPassword.Text = Settings.NetworkPassword;
+        }
+
+        private void PollProcesses()
+        {
+            while (true)
+            {
+                UpdateProtection?.Invoke();
+                System.Threading.Thread.Sleep(250);
+            }
+        }
+
+        private static byte[] ExtractResource(String filename)
+        {
+            System.Reflection.Assembly a = System.Reflection.Assembly.GetExecutingAssembly();
+            using (Stream resFilestream = a.GetManifestResourceStream(filename))
+            {
+                if (resFilestream == null) return null;
+                byte[] ba = new byte[resFilestream.Length];
+                resFilestream.Read(ba, 0, ba.Length);
+                return ba;
+            }
+        }
+
+        ProcessModuleEx moduleHandle = null;
+
+        private List<int> pidsOwned = new List<int>();
+        private void __updateprotection()
+        {
+            try
+            {
+                if (BlackOps3.Game != null)
+                {
+
+                    // check if the game has exited
+                    if (BlackOps3.Game.BaseProcess.HasExited)
+                    {
+                        throw new Exception("Black Ops 3 process");
+                    }
+
+                    do
+                    {
+                        // protect a new process if found
+                        if (!pidsOwned.Contains(BlackOps3.Game.BaseProcess.Id))
+                        {
+                            if (BlackOps3.Game.GetValue<long>(BlackOps3.Game[BlackOps3.Constants.OFF_GAME_READY]) == 0)
+                            {
+                                break;
+                            }
+
+                            if (!(BlackOps3.Game["zbr.dll"] is null)) // dont inject when its a zbr process
+                            {
+                                break;
+                            }
+
+                            //if((BlackOps3.Game.GetValue<byte>(BlackOps3.Game[0x162E4410]) & 1) != 1) // ui loaded once
+                            //{
+                            //    break;
+                            //}
+
+                            //// disable voice packet dispatching
+                            BlackOps3.SetDvar("maxvoicepacketsperframe", "0");
+
+                            //// disable callvote
+                            BlackOps3.SetDvar("sv_mapswitch", "0");
+
+                            if (true)
+                            {
+                                var bytes = ExtractResource("t7dwidm_protect.discord_game_sdk.dll");
+                                File.WriteAllBytes(Path.Combine(Path.GetDirectoryName(BlackOps3.Game.BaseProcess.MainModule.FileName), "discord_game_sdk.dll"), bytes);
+                                moduleHandle = BlackOps3.Game.LoadAndRegisterDllRemote(Path.Combine(Environment.CurrentDirectory, "t7patch.dll"));
+                                BlackOps3.Game.LoadAndRegisterDllRemote(Path.Combine(Environment.CurrentDirectory, "zbr2.dll"));
+                                BlackOps3.Game.LoadAndRegisterDllRemote(Path.Combine(Environment.CurrentDirectory, "BO3-Mod-Check-Patch.dll"));
+                            }
+                            BlackOps3.Game.Call(moduleHandle["zbr_run_gamemode_lui"], "serious_anticrash_2023");
+                            BlackOps3.Game.Call(moduleHandle["SetPlayerName"], Settings.Playername);
+                            BlackOps3.Game.Call(moduleHandle["SetFriendsOnly"], Settings.IsFriendsOnly);
+                            BlackOps3.Game.Call(moduleHandle["SetNetworkPassword"], Settings.NetworkPassword);
+
+                            this.Invoke((MethodInvoker)(() =>
+                            {
+                                lbl_Status.Text = "Protected Process with ID: " + BlackOps3.Game.BaseProcess.Id;
+                            }));
+                            pidsOwned.Add(BlackOps3.Game.BaseProcess.Id);
+                        }
+                    } while (false);
+                }
+            }
+            catch (Exception e)
+            {
+                moduleHandle = null;
+                if (!e.ToString().Contains("Black Ops 3 process"))
+                {
+                    if (e.ToString().Contains("Cannot process request because the process") && e.ToString().Contains("has exited."))
+                    {
+                        this.Invoke((MethodInvoker)(() =>
+                        {
+                            lbl_Status.Text = "Process has been exited.";
+                        }));
+                    }
+                    else
+                    {
+                        File.AppendAllText("errors.txt", e.ToString() + "\n\n");
+                        this.Invoke((MethodInvoker)(() =>
+                        {
+                            lbl_Status.Text = "An error has occurred. Please Check `errors.txt`";
+                        }));
+                    }
+                }
+                else
+                {
+                    // Update status label on the UI thread
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        lbl_Status.Text = "No game process found.";
+                    }));
+                }
+            }
+        }
+
+
+        private bool mouseDown;
+        private Point lastLocation;
+
+        private void Form_Dashboard_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void Form_Dashboard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void Form_Dashboard_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public class PlayerSettings
+        {
+            public string Playername { get; set; }
+            public bool IsFriendsOnly { get; set; }
+            public bool IsMTLPatchEnabled { get; set; }
+            public string NetworkPassword { get; set; }
+
+            public PlayerSettings()
+            {
+                Playername = "";
+                IsFriendsOnly = true;
+                IsMTLPatchEnabled = false;
+                NetworkPassword = "";
+            }
+        }
+
+        internal class LowerNamePol : JsonNamingPolicy
+        {
+            public override string ConvertName(string name) =>
+                name.ToLower();
+        }
+    }
+}
